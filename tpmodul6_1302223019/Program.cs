0177d274913835641979daee92027da981d515b0
purianagatha <purianagatha@gmail.com>
2024-03-25 05:13:24 +0700

    implementasi design by contract

--- tpmodul6_1302223019/Program.cs
+++ tpmodul6_1302223019/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace tpmodul6_1302223019
+﻿using System.Diagnostics;
+
+namespace tpmodul6_1302223019
 {
 
     public class SayaTubeVideo
@@ -11,12 +13,15 @@
         {
             Random random = new Random();
             this.id = random.Next(10000, 100000);
-            this.title = title;
+            Debug.Assert(title.Length <= 100 && title != null, "title tidak dapat dimasukkan");
+            this.title = checked(title);
             this.playCount = 0;
         }
         public void IncreasePlayCount(int playCount)
         {
-            this.playCount = this.playCount + playCount;
+            Debug.Assert(playCount < 10000000, "play count tidak dapat ditambahkan karena lebih dari 10.000.000");
+            Debug.Assert(this.playCount + playCount < int.MaxValue, "jumlah play count melebihi batas");
+            this.playCount = checked(this.playCount + playCount);
         }
         public void PrintVideoDetail()
         {
@@ -33,7 +38,11 @@
         static void Main(string[] args)
         {
             SayaTubeVideo video = new SayaTubeVideo("Tutorial Design By Contract - PURI LALITA ANAGATA");
-            video.IncreasePlayCount(1234567890);
+            // video.IncreasePlayCount(1234567890);
+            while (true)
+            {
+                video.IncreasePlayCount(1234567);
+            }
             video.PrintVideoDetail();
 
         }
